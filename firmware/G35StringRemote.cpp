58e6d42d25e9b0381555f313e94877130c527d72
Jeff Fisher <jeff@zaf.ca>
2014-11-12 21:07:26 -0600

    G35StringRemote -- no way in hell this works yet

---
+++ firmware/G35StringRemote.cpp
@@ -0,0 +1,112 @@
+/*
+  G35: An Arduino library for GE Color Effects G-35 holiday lights.
+
+  Copyright © 2011 The G35 Authors. Use, modification, and distribution are
+  subject to the BSD license as described in the accompanying LICENSE file.
+
+  Original version by Paul Martis (http://www.digitalmisery.com). See
+  README for complete attributions.
+
+  Special thanks to Richard <rkfoote@gmail.com> and "slinky" for the unrolled-
+  loop protocol code that seems to work very well!
+*/
+
+#include "G35StringRemote.h"
+
+G35String::G35String(uint8_t pin, uint8_t light_count,
+                     uint8_t physical_light_count,
+                     uint8_t bulb_zero, bool is_forward, char ip[], int port)
+: G35(), pin_(pin), physical_light_count_(physical_light_count),
+  bulb_zero_(bulb_zero), is_forward_(is_forward) {
+  light_count_ = light_count;
+  strncpy(ip_, ip, sizeof(ip_);
+  ip_[sizeof(ip_) - 1] = '\0';
+  client_.begin();
+}
+
+G35String::G35String(uint8_t pin, uint8_t light_count, char ip[], int port)
+: G35(), pin_(pin), physical_light_count_(light_count),
+  bulb_zero_(0), is_forward_(true) {
+  light_count_ = light_count;
+  strncpy(ip_, ip, sizeof(ip_);
+  ip_[sizeof(ip_) - 1] = '\0';
+  client_.begin();
+}
+
+void G35String::set_color(uint8_t bulb, uint8_t intensity, color_t color) {
+  bulb += bulb_zero_;
+
+  if (intensity > MAX_INTENSITY) {
+    intensity = MAX_INTENSITY;
+  }
+
+  char fmt[14];
+  sprintf(fmt, "S%02d%03d%03d%04d", pin_, bulb, intensity, color);
+  fmt[sizeof(fmt) - 1] = '\0';
+ 
+  client_.beginPacket(ip_, port_);
+  client_.write(fmt);
+  client_.endPacket();
+}
+
+void G35String::enumerate() {
+  enumerate(is_forward_);
+}
+
+void G35String::enumerate(bool forward) {
+  uint8_t count = physical_light_count_;
+  uint8_t bulb = forward ? 0 : light_count_ - 1;
+  int8_t delta = forward ? 1 : -1;
+  while (count--) {
+    set_color(bulb, MAX_INTENSITY, COLOR_RED);
+    bulb += delta;
+  }
+}
+
+void G35String::enumerate_forward() {
+  enumerate(true);
+}
+
+void G35String::enumerate_reverse() {
+  enumerate(false);
+}
+
+void G35String::do_test_patterns() {
+  const uint8_t last_light = light_count_ - 1;
+
+  // Cycle through primaries.
+  fill_color(0, light_count_, MAX_INTENSITY, COLOR_RED);
+  delay(1000);
+  fill_color(0, light_count_, MAX_INTENSITY, COLOR_GREEN);
+  delay(1000);
+  fill_color(0, light_count_, MAX_INTENSITY, COLOR_BLUE);
+  delay(1000);
+
+  // Tickle the ends. You should see three reds at the start, and three greens
+  // at the end. This confirms that you've properly configured the strand
+  // lengths and directions.
+  for (int i = 0; i < 8; ++i) {
+    for (int j = 0; j < 3; ++j) {
+      set_color(BROADCAST_BULB, 0, COLOR_BLACK);
+      set_color(j, MAX_INTENSITY, COLOR_RED);
+      set_color(last_light - j, MAX_INTENSITY, COLOR_GREEN);
+      delay(250);
+    }
+  }
+
+  // Full white for five seconds. If you have power problems, they'll probably
+  // show up here.
+  fill_color(0, light_count_, MAX_INTENSITY, COLOR_WHITE);
+  delay(5000);
+
+  // Cycle through secondaries.
+  fill_color(0, light_count_, MAX_INTENSITY, COLOR_YELLOW);
+  delay(1000);
+  fill_color(0, light_count_, MAX_INTENSITY, COLOR_CYAN);
+  delay(1000);
+  fill_color(0, light_count_, MAX_INTENSITY, COLOR_MAGENTA);
+  delay(1000);
+
+  // ... and get ready for the first program.
+  fill_color(0, light_count_, MAX_INTENSITY, COLOR_BLACK);
+}
---
+++ firmware/G35StringRemote.h
@@ -0,0 +1,71 @@
+/*
+  G35: An Arduino library for GE Color Effects G-35 holiday lights.
+  Copyright © 2011 The G35 Authors. Use, modification, and distribution are
+  subject to the BSD license as described in the accompanying LICENSE file.
+
+  Original version by Paul Martis (http://www.digitalmisery.com). See
+  README for complete attributions.
+*/
+
+#ifndef INCLUDE_G35_STRINGREMOTE_H
+#define INCLUDE_G35_STRINGREMOTE_H
+
+#include "G35.h"
+
+// A G35String knows how to talk to a real GE Color Effects light string.
+// In particular, it implements the set_color() method of the G35 interface.
+//
+// These strings need enumerate() to be called before anything else, so that
+// each bulb has an individual address. In many cases, your lights will work
+// as expected even if you forget to call enumerate(), because most programs
+// happen to do the same thing as enumerate() -- namely, they send commands
+// to all bulbs starting with bulb #0 and ending with bulb #N-1. If your
+// light programs look right but fractured, it's because you forgot to call
+// enumerate().
+class G35StringRemote : public G35 {
+ public:
+  // |pin|: the Arduino pin driving this string's data line.
+  // |light_count|: the number of visible, physical bulbs on the string.
+  // |physical_light_count|: the number of physical bulbs on the string.
+  // |bulb_zero|: the index of the first bulb (almost always zero).
+  // |is_forward|: true if the closest bulb to the plug has the lowest index.
+  G35String(uint8_t pin, uint8_t light_count, uint8_t physical_light_count,
+            uint8_t bulb_zero, bool is_forward, char ip[], int port);
+  G35String(uint8_t pin, uint8_t light_count, char ip[], int port);
+
+  // Implementation of G35 interface.
+  virtual uint16_t get_light_count() { return light_count_; }
+  void set_color(uint8_t led, uint8_t intensity, color_t color);
+
+  // Initialize lights by giving them each an address.
+  void enumerate();
+
+  // Displays known-good patterns. Useful to prevent insanity during hardware
+  // debugging.
+  void do_test_patterns();
+
+ protected:
+  virtual uint8_t get_broadcast_bulb() { return BROADCAST_BULB; }
+
+ private:
+  uint8_t pin_;
+  uint8_t physical_light_count_;
+  uint8_t bulb_zero_;
+  bool is_forward_;
+  char ip_[16];
+  int port_;
+
+  enum {
+    MAX_INTENSITY = 0xcc,
+    BROADCAST_BULB = 63,
+  };
+
+  // Initialize lights by giving them each an address. enumerate_forward()
+  // numbers the bulb closest to the controller 0, and enumerate_reverse()
+  // numbers the farthest bulb 0.
+  void enumerate(bool reverse);
+  void enumerate_forward();
+  void enumerate_reverse();
+};
+
+#endif  // INCLUDE_G35_STRINGREMOTE_H
